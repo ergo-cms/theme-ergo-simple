ed6a20ab1896fbb3d56f8afe8e836c59ae50c20c
cmroanirgo <cmroanirgo@users.noreply.github.com>
2017-01-26 07:18:45 +1100

    improved support for featured & popular posts

--- theme.ergo.js
+++ theme.ergo.js
@@ -1,5 +1,5 @@
 /*
-This is a minimal skeleton config for ergo cms. See online for more options
+This is the erg-simple theme config for ergo cms. See online for more options
 */
 
 module.exports = {
@@ -7,41 +7,13 @@ module.exports = {
 	url: "https://github.com/ergo-cms/theme-ergo-simple",
 	asset_paths: ['styles.css','favicon.ico','images','flaticon'],
 	default_fields: {
-		dynamic_list: function() { // See*** below for description
-			var list = this[this.list_type || 'page'].slice(0, this.list_len || 999);
-
-			if (list.length>0){
-				if (list[0].first === undefined)
-					list[0].first = true;
-				if (list[list.length-1].last === undefined)
-					list[list.length-1].last = true;
-			}
-			return list;
-		}
+		image: 'post.jpg',
+		author_image: 'avatar.jpg',
+		author_url: '/authors.html',
+		tags_url: '/tags.html',
+		categories_url: '/categories.html',
+		feed_url: '/rss.xml'
 	}
 };
 
 
-
-
-/**
-
-// LISTS
-// This bit of strangess work like this:
-
-1. In the main part of the website, there's page (say posts.tex) that has this in the header:
-	layout = list.html
-	list_type = post
-	list_len = 100
-
-2. At render time, list.html has access to 'list_type' and 'list_len'. 
-	This theme then has defined the 'list.html' layout as:
-		{{#dynamic_list}} ... {{/dynamic_list}}
-
-3. The system then ends up at the 'dynamic_list' function above, which returns the array given: 
-	eg. return this['post'], which comes from elsewhere step 1.
-
-
-Why? This allows a theme to make theme-centric lists of posts/authors, tags, etc, and 
-	frees up the user from worrying about such things, when changing themes
-*/
